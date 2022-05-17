da8fe34e701783008625cebfa73722cc8fe7039d
ivettepinto7 <00186319@uca.edu.sv>
2022-05-17 00:10:33 -0600

    Modify: system modulation
    Validate path application depending on user role
    
    Co-authored-by: Oscar Rodriguez 00206019@uca.edu.sv Oscar Recinos 00128519@uca.edu.sv

--- Client/holistic-hospital/src/helpers/Routes.js
+++ Client/holistic-hospital/src/helpers/Routes.js
@@ -7,59 +7,76 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/usuarios');
             },
+            path: '/landing/usuarios',
             roles: [UserRoles.ADMINISTRADOR, UserRoles.SECRETARIA],
         },
         {
             label: 'Expediente',
+            path: '/landing/expediente',
             roles: [UserRoles.USUARIO],
             items: [
                 {
                     label: 'Inmunizaciones',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/expediente/inmunizaciones');
-                    }
+                    },
+                    pathi: '/landing/expediente/inmunizaciones',
                 },
                 {
                     label: 'Exámenes realizados',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/expediente/examenes-realizados');
                     },
+                    pathi: '/landing/expediente/examenes-realizados',
                 },
                 {
                     label: 'Recetas médicas',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/expediente/recetas-medicas');
                     },
+                    pathi: '/landing/expediente/recetas-medicas',
                 },
                 {
                     label: 'Citas previas',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/expediente/citas-previas');
                     },
+                    pathi: '/landing/expediente/citas-previas',
                 },
             ]
         },
         {
             label: 'Citas',
             roles: [UserRoles.USUARIO],
+            path: '/landing/citas',
             items: [
                 {
                     label: 'Agendar cita médica',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/citas/cita-medica');
                     },
+                    pathi: '/landing/citas/cita-medica',
                 },
                 {
                     label: 'Agendar examen',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/citas/cita-examenes');
                     },
+                    pathi: '/landing/citas/cita-examenes',
                 },
                 {
                     label: 'Agendar inmunización',
+                    rolesi: [UserRoles.USUARIO],
                     command: (event) => {
                         navigator('/landing/citas/cita-inmunizacion');
                     },
+                    pathi: '/landing/citas/cita-inmunizacion',
                 },
             ]
         },
@@ -69,6 +86,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/recordatorios');
             },
+            path: '/landing/recordatorios',
         },
         {
             label: 'Agregar examen',
@@ -76,6 +94,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/agregar-examen');
             },
+            path: '/landing/agregar-examen',
         },
         {
             label: 'Agregar vacuna',
@@ -83,6 +102,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/agregar-vacuna');
             },
+            path: '/landing/agregar-vacuna',
         },
         {
             label: 'Agregar área de hospital',
@@ -90,6 +110,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/agregar-area');
             },
+            path: '/landing/agregar-area',
         },
         {
             label: 'Agregar medicamento',
@@ -97,6 +118,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/agregar-medicamento');
             },
+            path: '/landing/agregar-medicamento',
         },
         {
             label: 'Asignar turnos',
@@ -104,6 +126,7 @@ export function convertRoutes(navigator) {
             command: (event) => {
                 navigator('/landing/asignar-turnos');
             },
+            path: '/landing/asignar-turnos',
         },
     ]
 }
--- Client/holistic-hospital/src/pages/Landing.js
+++ Client/holistic-hospital/src/pages/Landing.js
@@ -1,19 +1,75 @@
 import React, { useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext/UserContext";
 
 //Components imports
 import SideBar from "../components/SideBar";
 
+//Helpers imports
+import { convertRoutes } from "../helpers/Routes";
+import { Roles } from '../helpers/Roles';
+
 export default function Landing() {
     const navigate = useNavigate();
-    const { token } = useContext(UserContext);
+    const { token, getUserStorage } = useContext(UserContext);
+    const { rol } = getUserStorage();
     const isLogged = token !== '';
 
+    const usePathname = () => {
+        const location = useLocation();
+        return location.pathname;
+    };
+
+    const pathname = usePathname();
+    const routes = convertRoutes(navigate);
+
+    const castRol = Roles.find((r) => {
+        return r.code === rol;
+    });
+
     useEffect(() => {
-        if(!isLogged) navigate("/");
-    },[isLogged, navigate]);
+        if (!isLogged) navigate("/");
+        else {
+            const fixedRoutes = routes.map(({ path, ...rest }) => {
+                if (rest.items !== undefined) {
+                    rest.items.map(({ pathi, ...data }) => {
+                        return ({
+                            ...data,
+                            pathi: `${pathi}`
+                        })
+                    })
+                }
+                return ({
+                    ...rest,
+                    path: `${path}`
+                })
+            });
+            fixedRoutes.forEach(({ path, roles, items }) => {
+                if (items !== undefined) {
+                    items.forEach(({ pathi, rolesi }) => {
+                        let match = pathi === pathname ? true : false;
+                        console.log("Match",match);
+                        if (match === true) {
+                            const authorized = rolesi.some((rol) => rol === castRol.name);
+                            console.log("AUTH", authorized);
+                            if (authorized === false)
+                                navigate("/landing");
+                        }
+                        })
+                } else {
+                    let match = path === pathname ? true : false;
+                    console.log("Match", match);
+                    if (match === true) {
+                        const authorized = roles.some((rol) => rol === castRol.name);
+                        console.log("AUTH", authorized);
+                        if (authorized === false)
+                            navigate("/landing");
+                    }
+                }
+            })
+        }
+    }, [isLogged, navigate, pathname, rol, routes, castRol.name]);
 
     return (
         <div className="w-full h-screen">
